46e44ca8ccbb798db4c356958974982ab2a413c2
Tharukie-0109 <tharukiediwyanjali@gmail.com>
2026-06-05 07:23:37 -0700

    Add files via upload

---
+++ tharuz.js
@@ -0,0 +1,10 @@
+function hello() {
+	alert ("hello student!");
+}
+function changecolor(){
+	document.getElementById("text").style.color="red";
+}	
+function changebackground(){
+	document.body.style.backgroundColor="yellow";
+}	
+	
